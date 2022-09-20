d41efbcbd2a192a6853e1774e1cd272acf3ed0a5
Tuan Anh <anh.nguyen2@ekoios.vn>
2022-09-20 18:22:19 +0700

    add check role

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,19 +1,22 @@
 import { Body, Controller, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ApiError } from 'src/common/api';
-import { ErrorCode } from 'src/common/constants';
+import { Role } from '../auth/role.enum';
+import { Roles } from '../auth/roles.decorator';
 import { ApiSuccessResponse } from 'src/common/response/api-success';
 import { GetUser } from '../auth/get-user.decorator';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { AdminService } from './admin.service';
 import { CreateAdminDto, FilterAdminDto } from './dto/admin.dto';
+import { RolesGuard } from '../auth/guard/roles.guard';
 @Controller('admin')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('Admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.SuperAdmin)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Filter Admins' })
   async findAll(
@@ -26,12 +29,13 @@ export class AdminController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.SuperAdmin)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Create admin' })
   async createAdmin(
     @Body() createAdminDto: CreateAdminDto,
     @GetUser() user,
-  ) {
+  ) {    
     const data = await this.adminService.createAdmin(user, createAdminDto);
     return new ApiSuccessResponse().success(data, '')
   }
