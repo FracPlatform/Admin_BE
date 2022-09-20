8a03e32d87f9afba05768163b45344b9878354cc
Jacky Nguyen <hoang.nguyen@ekoios.vn>
2022-09-20 14:58:25 +0700

    update validate edit fractor

--- src/modules/fractor/dto/update-fractor.dto.ts
+++ src/modules/fractor/dto/update-fractor.dto.ts
@@ -1,24 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, Min } from 'class-validator';
 
 export class UpdateFractorDto {
   @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
+  @MaxLength(16)
   assignedBD: string;
 
   @ApiProperty({ required: false })
   @IsNumber({ allowNaN: true, maxDecimalPlaces: 2 })
   @IsOptional()
+  @Min(0)
   iaoFeeRate: number;
 
   @ApiProperty({ required: false })
   @IsNumber({ allowNaN: true, maxDecimalPlaces: 2 })
   @IsOptional()
+  @Min(0)
   tradingFeeProfit: number;
 
   @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
+  @MaxLength(2000)
   deactivationComment: string;
 }
