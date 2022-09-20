c4d7a45856d193969f8575a1aec88de2d6d8abed
Cris Nguyen <cuong.nguyen@ekoios.vn>
2022-09-20 18:08:27 +0700

    first approve iao request

--- src/datalayer/model/iao-request.model.ts
+++ src/datalayer/model/iao-request.model.ts
@@ -4,8 +4,8 @@ export type IAORequestDocument = IAORequest & Document;
 
 export class ApprovedBy {
   adminId: string;
-  status: string;
-  comment:string;
+  status: number;
+  comment: string;
   createdAt: Date;
 }
 
@@ -14,6 +14,7 @@ export enum IAO_REQUEST_TYPE {
   NON_VAULT = 2,
 }
 
+export const MAX_IAO_REQUEST_COMMENT = 1000;
 export const MAX_IAO_REQUEST_NOTE = 3000;
 export const MAX_LENGTH_PHONE = 16;
 export const MAX_IAO_REQUEST_ADDRESS = 512;
