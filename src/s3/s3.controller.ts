ae4c3e1d9ca7a84cb35b17f0acf55c937edff90d
Đạt Nguyễn Thành <datthanh@ats-MacBook-Pro.local>
2022-09-22 14:06:31 +0700

    fix/add search document api

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -1,28 +1,29 @@
-import { Controller, Get, Query, Req } from '@nestjs/common';
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { ApiSuccessResponse } from 'src/common/response/api-success';
+import { JwtAuthGuard } from 'src/modules/auth/guard/jwt-auth.guard';
 import { PresignUrlDto } from './dto/s3.dto';
 import { S3Service } from './s3.service';
 
 @Controller('s3')
 @ApiTags('s3')
 export class S3Controller {
-  constructor(private readonly s3Service: S3Service) { }
+  constructor(private readonly s3Service: S3Service) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'get presigned URL' })
   @ApiBearerAuth()
-  async create(
-    @Query() presignUrlDto: PresignUrlDto,
-    @Req() req: Request,
-  ) {
+  async create(@Query() presignUrlDto: PresignUrlDto, @Req() req: Request) {
     try {
-      const result = await this.s3Service.getPresignedUrl(presignUrlDto, req.user);
+      const result = await this.s3Service.getPresignedUrl(
+        presignUrlDto,
+        req.user,
+      );
       return new ApiSuccessResponse().success(result, '');
     } catch (error) {
       throw error;
     }
   }
-
 }
