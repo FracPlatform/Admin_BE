0b5b0adec804823fbdd7359e1c6ce621556401f6
Đạt Nguyễn Thành <datthanh@ats-MacBook-Pro.local>
2022-10-19 15:05:37 +0700

    feat/add apidex

--- src/modules/dex/dex.module.ts
+++ src/modules/dex/dex.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { DataServicesModule } from 'src/services';
 import { DexController } from './dex.controller';
 import { DexService } from './dex.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_DEX_SECRET,
+    }),
+    DataServicesModule,
+  ],
   controllers: [DexController],
-  providers: [DexService]
+  providers: [DexService, JwtStrategy],
 })
 export class DexModule {}
