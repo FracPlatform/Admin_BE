87f12752e80aed417720b2f2b773ee188d55f1c3
Đạt Nguyễn Thành <datthanh@ats-MacBook-Pro.local>
2022-11-03 10:39:38 +0700

    fix/fix validate min max percentage

--- src/modules/revenue/dto/approve-iao-revenue.dto.ts
+++ src/modules/revenue/dto/approve-iao-revenue.dto.ts
@@ -3,14 +3,14 @@ import { IsNumber, Max, Min } from 'class-validator';
 
 export class ApproveIaoRevenueDto {
   @IsNumber()
-  @Max(99)
-  @Min(0)
+  @Max(99.99)
+  @Min(0.01)
   @ApiProperty({ type: Number })
   platformCommissionRate: number;
 
   @IsNumber()
-  @Max(99)
-  @Min(0)
+  @Max(99.99)
+  @Min(0.01)
   @ApiProperty({ type: Number })
   bdCommissionRate: number;
 }
