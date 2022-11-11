c6578f96d6174361b710a8f0bc937852f59ca654
Đạt Nguyễn Thành <datthanh@ats-MacBook-Pro.local>
2022-11-11 13:52:03 +0700

    fix/update filter list nft

--- src/providers/worker/worker.service.ts
+++ src/providers/worker/worker.service.ts
@@ -856,6 +856,8 @@ export class WorkerService {
         },
         {
           status: WITHDRAWAL_REQUEST_STATUS.SUCCESSFUL,
+          txHash: requestData.transactionHash,
+          transactionCompletedOn: new Date(),
         },
         {
           session,
